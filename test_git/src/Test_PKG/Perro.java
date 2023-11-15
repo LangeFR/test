aeed9b0ea2493d63412ec648de98118159a7643c
Mateo Vanegas <mdjvanegasc@gmail.com>
2023-11-15 10:21:21 -0500

    Cree variables y constructor

--- test_git/src/Test_PKG/Perro.java
+++ test_git/src/Test_PKG/Perro.java
@@ -1,5 +1,18 @@
 package Test_PKG;
 
+
 public class Perro {
-    
+    private String nombre;
+    public int correo;
+    private int nickName;
+
+    Perro(String nombre, int correo, int nickName){
+        nombre = nombre;
+        correo = correo;
+        nickName = nickName;
+    }
+
+    public String getNombre(){return nombre;}
+
+
 }
